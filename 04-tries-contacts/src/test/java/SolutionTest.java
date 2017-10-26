00d45ce6fbe3f302cc4c5fe8b03eed325e5d9b1a
Zsolt Fabok <me@zsoltfabok.com>
2017-10-26 21:48:26 +0200

    add tries contact test

--- 04-tries-contacts/src/test/java/SolutionTest.java
+++ 04-tries-contacts/src/test/java/SolutionTest.java
@@ -1,7 +1,40 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
 public class SolutionTest {
+    private InputStream origIn;
+    private PrintStream origOut;
+
+    @Before
+    public void setup() {
+        origIn = System.in;
+        origOut = System.out;
+    }
+
+    @After
+    public void teardown() {
+        System.setOut(origOut);
+        System.setIn(origIn);
+    }
+
     @Test
-    public void foo() {
+    public void basic() throws Exception {
+        String input = "4\nadd hack\nadd hackerrank\nfind hac\nfind hak";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        Solution.main(null);
+
+        assertEquals("2\n0\n", out.toString());
     }
 }
