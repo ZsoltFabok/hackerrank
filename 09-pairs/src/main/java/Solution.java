9cd9a14520d047027e4a0d5a85c164f2638e2b75
Zsolt Fabok <me@zsoltfabok.com>
2017-10-30 22:45:33 +0100

    pairs

---
+++ 09-pairs/src/main/java/Solution.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] a = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+
+        System.out.println(numberOfPairs(a, k));
+    }
+
+    static int numberOfPairs(int[] array, int k) {
+        Map<Integer, Integer> cache = new HashMap<>();
+        int count = 0;
+        for (int number : array) {
+            // store number in cache
+            if (!cache.containsKey(number)) {
+                cache.put(number, 0);
+            }
+            cache.put(number, cache.get(number) + 1);
+
+            // Check for pairs in the cache, and increase counter
+            // with the current occurrences of the the pair
+            // if number is 3, and k is 2, the possible pairs are (1, 3) and (3, 5)
+            // Check how many ones and fives are in the cache and count them.
+            if (cache.containsKey(number - k)) {
+                count += cache.get(number - k);
+            }
+            if (cache.containsKey(number + k)) {
+                count += cache.get(number + k);
+            }
+        }
+        return count;
+    }
+}
